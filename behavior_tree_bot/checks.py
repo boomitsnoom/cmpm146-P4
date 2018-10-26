8cab3fc4c07d674b4c6a8a9e687f18b4b66b1b1c
Naum Markenzon <nmarkenz@ucsc.edu>
2018-10-25 23:49:53 -0700

    Added both attack behaviors

--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -9,3 +9,19 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
+def if_neutral_planet_with_high_growth_5(state):
+		return any(state.neutral_planets().growth_rate(5))
+
+def if_neutral_planet_with_high_growth_4(state):
+		return any(state.neutral_planets().growth_rate(4))
+
+def if_neutral_planet_with_high_growth_3(state):
+		return any(state.neutral_planets().growth_rate(3))
+
+def if_neutral_planet_with_high_growth_2(state):
+		return any(state.neutral_planets().growth_rate(2))
+
+def if_neutral_planet_with_high_growth_1(state):
+		return any(state.neutral_planets().growth_rate(1))
+
